021c9bfb3bc9d9a4016582f6b500cb518afb4d0d
alexandervillarroel0078 <villarroelalexander215@gmail.com>
2025-04-11 19:28:41 -0400

    fix: actualización de conexión a la base de datos

--- config.py
+++ config.py
@@ -1,17 +1,14 @@
+import os
 import psycopg2
 
 def conectar_db():
     try:
-        conexion = psycopg2.connect(
-            host="localhost",
-            database="smart_cart",
-            user="postgres",
-            password=1234
-        )
-        print("✅ Conexión exitosa a PostgreSQL")
+        db_url = os.getenv("DATABASE_URL")  # Render provee esta variable
+        conexion = psycopg2.connect(db_url)
+        print("✅ Conectado a PostgreSQL en Render")
         return conexion
     except Exception as e:
-        print("❌ Error al conectar a PostgreSQL:", e)
+        print("❌ Error al conectar con la base en Render:", e)
         return None
 
 # ✅ Clave secreta para JWT
